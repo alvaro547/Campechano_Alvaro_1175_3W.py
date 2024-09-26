39b6f07025e9918bc0f7faa6070bc12932767fd7
alvaro547 <a.23308051281175@cbtis128.edu.mx>
2024-09-26 15:44:49 -0600

    Add files via upload

---
+++ Campechano_Alvaro_1175_3W/Campechano_Alvaro_1175_3W.py
@@ -0,0 +1,21 @@
+(" ")
+print("Campechano_Alvaro_1175_3W")
+print(" ")
+#Capturar el nombre y el apellido
+Apellido_Paterno = (input("ingresa el apellido paterno"))
+Apellido_Materno = (input("ingresa el apellido materno"))
+Nombre_ = (input("ingresa tu nombre"))
+print(" ")
+#desplegar las variables guardadas
+print("apellido paterno: "+Apellido_Paterno)
+print("apellido materno: "+Apellido_Materno)
+print("nombre: "+Nombre_)
+print(" ")
+# desplegar el nombre completo y con mayusculas
+Nombre_Completo = Nombre_ + " " + " "+Apellido_Paterno+" "+" "+Apellido_Materno
+print("tu nombre completo es: "+ Nombre_Completo)
+print(" ")
+#desplegar nombre completo en mayusculas
+Nombre_Completo = Nombre_Completo.upper()
+print("tu nombre completo en mayusculas es: " +Nombre_Completo)
+print(" ")
